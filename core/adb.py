f08cacf66b92e6334b7b49a7a1df3dc1b199b3d0
Jacob Kang <jacob@litbig.com>
2021-07-23 17:14:04 +0900

    adb test 항목 추가

--- core/adb.py
+++ core/adb.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import time
 from collections import OrderedDict
 
@@ -13,95 +14,98 @@ class Adb:
         self.__project = _project
         self.__json = _project.get_path()
 
+    def command(self, _cmd: str):
+        return subprocess.check_output(_cmd, shell=True).decode('utf-8').strip()
+
     def remount(self):
-        os.system('adb remount')
+        return self.command('adb remount')
 
     def reboot(self, _reason=''):
         if len(_reason) > 0:
-            os.system('adb reboot {reason}'.format(reason=_reason))
-            return
-        os.system('adb reboot')
+            return os.system('adb reboot {reason}'.format(reason=_reason))
+
+        return os.system('adb reboot')
 
     def push(self, _dir, _target):
         from core import utils
 
-        os.system('adb push {root}{out}{dir}{target} {dir}'
-                  .format(root=self.__json.get(utils.FROM),
-                          out=self.__json.get(utils.TO),
-                          dir=_dir,
-                          target=_target))
+        return os.system('adb push {root}{out}{dir}{target} {dir}'
+                         .format(root=self.__json.get(utils.FROM),
+                                 out=self.__json.get(utils.TO),
+                                 dir=_dir,
+                                 target=_target))
 
     def install(self, _dir, _target):
         from core import utils
 
-        os.system('adb install -r {root}{out}{dir}{target}/{target}.apk'
-                  .format(root=self.__json.get(utils.FROM),
-                          out=self.__json.get(utils.TO),
-                          dir=_dir,
-                          target=_target))
+        return os.system('adb install -r {root}{out}{dir}{target}/{target}.apk'
+                         .format(root=self.__json.get(utils.FROM),
+                                 out=self.__json.get(utils.TO),
+                                 dir=_dir,
+                                 target=_target))
 
     def lib_push(self, _target):
         from core import utils
 
-        self.push(utils.LIB_DIR, _target)
+        return self.push(utils.LIB_DIR, _target)
 
     def framework_push(self, _target):
         from core import utils
 
-        self.push(utils.FRAMEWORK_DIR, _target)
+        return self.push(utils.FRAMEWORK_DIR, _target)
 
     def priv_app_push(self, _target):
         from core import utils
 
-        self.push(utils.PRIV_APP_DIR, _target)
+        return self.push(utils.PRIV_APP_DIR, _target)
 
     def app_push(self, _target):
         from core import utils
 
-        self.push(utils.APP_DIR, _target)
+        return self.push(utils.APP_DIR, _target)
 
     def priv_app_install(self, _target):
         from core import utils
 
-        self.install(utils.PRIV_APP_DIR, _target)
+        return self.install(utils.PRIV_APP_DIR, _target)
 
     def app_install(self, _target):
         from core import utils
 
-        self.install(utils.APP_DIR, _target)
+        return self.install(utils.APP_DIR, _target)
 
     def app_launch(self, _package):
-        os.system('adb shell monkey -p {pkg} -c android.intent.category.LAUNCHER 1'
-                  .format(pkg=_package))
+        return self.command('adb shell monkey -p {pkg} -c android.intent.category.LAUNCHER 1'
+                            .format(pkg=_package))
 
     def fastboot(self, _image):
         from core import utils
         from core.project import Project
 
         if _image == 'dtb' and self.__project.get_project() == Project.HLAB:
-            os.system('fastboot flash {img} {root}{out}tcc8030-android-lpd4321_sv0.1.dtb'
-                      .format(root=self.__json.get(utils.FROM),
-                              out=self.__json.get(utils.TO),
-                              img=_image))
+            return os.system('fastboot flash {img} {root}{out}tcc8030-android-lpd4321_sv0.1.dtb'
+                             .format(root=self.__json.get(utils.FROM),
+                                     out=self.__json.get(utils.TO),
+                                     img=_image))
 
         else:
-            os.system('fastboot flash {img} {root}{out}{img}.img'
-                      .format(root=self.__json.get(utils.FROM),
-                              out=self.__json.get(utils.TO),
-                              img=_image))
+            return os.system('fastboot flash {img} {root}{out}{img}.img'
+                             .format(root=self.__json.get(utils.FROM),
+                                     out=self.__json.get(utils.TO),
+                                     img=_image))
 
     def fastboot_reboot(self):
-        os.system('fastboot reboot')
+        return os.system('fastboot reboot')
 
     def screen_capture(self) -> str:
         from core import log
 
         __format = '%Y%m%d_%H%M%S.png'
-        __result = time.strftime(__format, time.localtime())
-        os.system('adb shell screencap -p /data/{0}'.format(__result))
-        os.system("adb pull /data/{0} {1}/Downloads".format(__result, os.getenv('HOME')))
+        __file = time.strftime(__format, time.localtime())
+        os.system('adb shell screencap -p /data/{0}'.format(__file))
+        os.system("adb pull /data/{0} {1}/Downloads".format(__file, os.getenv('HOME')))
 
-        out_path = '{0}/Downloads/{1}'.format(os.getenv('HOME'), __result)
+        out_path = '{0}/Downloads/{1}'.format(os.getenv('HOME'), __file)
 
         if os.path.exists(out_path) is False:
             log.e('screen capture fail')
@@ -111,13 +115,13 @@ class Adb:
         return out_path
 
     def broadcast(self, _what: str):
-        os.system('adb shell am broadcast -a {}'.format(
+        return self.command('adb shell am broadcast -a {}'.format(
             {'boot_completed': 'com.litbig.action.BOOT_COMPLETED'}.get(_what)))
 
     def key_event(self, _what: str):
-        os.system('adb shell input keyevent KEYCODE_{}'.format(_what.upper()))
+        return os.system('adb shell input keyevent KEYCODE_{}'.format(_what.upper()))
 
     def version_name(self, _what: str):
-        os.system('adb shell dumpsys package {} | grep versionName'.format(
+        return self.command('adb shell dumpsys package {} | grep versionName'.format(
             {'polnav': 'com.polstar.polnav6',
              'launcher': 'hanhwa.lm18i.launcher'}.get(_what)))
--- test/test_adb.py
+++ test/test_adb.py
@@ -14,8 +14,28 @@ def adb():
     return Adb(Project())
 
 
+def test_remount(adb):
+    assert adb.remount() == 'remount succeeded'
+
+
+def test_launch(adb):
+    assert ('bash arg: hanhwa.lm18i.launcher' in adb.app_launch('hanhwa.lm18i.launcher')) is True
+
+
 def test_capture(adb):
     path = adb.screen_capture()
     print('path = {}'.format(path))
     print('exist = {}'.format(os.path.exists(path)))
     assert os.path.exists(path) is True
+
+
+def test_key_event(adb):
+    assert adb.key_event('HOME') is 0
+
+
+def test_version_name(adb):
+    assert adb.version_name('polnav') == 'versionName=134641C V.6383'
+
+
+def test_broadcast(adb):
+    assert ('Broadcast completed: result=0' in adb.broadcast('boot_completed')) is True
